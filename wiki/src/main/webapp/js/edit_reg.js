12cfb5b7f244bc8b78ef64a855016b7b7bef9745
jsk2606 <jsk26060@gmail.com>
2022-04-05 14:04:41 +0900

    `

--- wiki/src/main/webapp/js/edit_reg.js
+++ wiki/src/main/webapp/js/edit_reg.js
@@ -1,35 +1,9 @@
 //저장 기능
 function save(){
-	var editCode = $('#summernote').summernote('code');
+	var editCode = $('#summernote').summernote('code');	//에디터 입력내용
 	
 	//데이터 저장 요청
-	fetch(
-		'http://localhost:8080/v1/saveEdit',
-    {
-        method: 'post',
-        headers: { "Content-Type": "application/text; charset=UTF-8;" },
-        body: JSON.stringify({content : editCode})
-    })
-    .then(function (response) {
-        if (response.ok) {
-        	return response.json();
-        }
-    })
-    .then(function (data) {
-   		console.log(data);
-        if (data) 
-    	{
-        	//data.result : 0 이면 성공 , -1이면 실패
-        	if(data.result == "0"){
-        		alert('저장 되었습니다.');
-        	}else if(data.result == "-1"){
-        		alert(data.msg)
-        	}
-        
-    	}
-        else
-		console.log("error");
-     });
+	
 }
 
 
